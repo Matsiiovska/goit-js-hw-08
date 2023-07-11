6c3c1980e39ce4bd289d8653bc47ba132318f4cb
Matsiiovska <125771191+Matsiiovska@users.noreply.github.com>
2023-07-11 17:21:33 +0300

    const form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,7 +2,7 @@ import throttle from 'lodash.throttle';
 
 const LOCAL_KEY = 'feedback-form-state';
 
-let form = document.querySelector('.feedback-form');
+const form = document.querySelector('.feedback-form');
 
 form.addEventListener('input', throttle(inputData, 500));
 form.addEventListener('submit', formSubmit);
